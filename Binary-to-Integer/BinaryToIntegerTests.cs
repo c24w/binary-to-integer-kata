f68d79af3e2d002ecfb11713a2f1358e796e1f0c
Chris Watson <chris.watson@7digital.com>
2013-02-25 16:21:51 +0000

    Parse zero string to zero

---
+++ Binary-to-Integer/BinaryToIntegerTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Binary_to_Integer
+{
+	[TestFixture]
+    public class BinaryToIntegerTests
+	{
+		[Test]
+		public void Zero_string_should_return_zero()
+		{
+			var result = ParseBinary("0");
+			Assert.That(result, Is.EqualTo(0));
+		}
+
+		private int ParseBinary(string binary)
+		{
+			return 0;
+		}
+	}
+}
